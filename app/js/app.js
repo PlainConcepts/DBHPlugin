0d813802c84e96b83b11b5981b947e0603a4fb0b
Rodrigo Corral <rcorral@plainconcepts.com>
2014-10-10 09:37:40 +0200

    Create initial tests for historyController

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
     'use strict';
 
-    angular.module("DBHPluginApp", []);
+    angular.module('DBHPluginApp', []);
 
 
 }());
--- test/specs/app/historyControllerTest.js
+++ test/specs/app/historyControllerTest.js
@@ -1,1 +1,20 @@
-﻿
+﻿describe('historyControllerTest', function () {
+
+    var scope, controller;
+
+    beforeEach(function () {
+        module('DBHPluginApp');
+        window.chrome = {};
+    });
+
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+        controller = $controller('HistoryController', {
+            '$scope': scope
+        });
+    }));
+
+    it('gets the filtered history of urls', function () {
+        expect(scope.filteredUrl).not.toBe('');
+    });
+});
