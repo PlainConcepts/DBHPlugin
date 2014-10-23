bac9b575e695a4c6892429c1743d3b3b0fdea7e0
Hugo Biarge <hugo@arosbi.com>
2014-10-23 12:59:44 +0200

    Fix timezone sensible tests

--- test/specs/app/historyAnalyzerTest.js
+++ test/specs/app/historyAnalyzerTest.js
@@ -3,11 +3,19 @@
 describe('historyAnalyzer Test', function () {
 
     'use strict';
+    function getFriendlyTime(time) {
+        return moment(time).format('LT');
+    }
+
+    function getFriendlyDate(time) {
+        return moment(time).format('dddd, MMMM D YYYY');
+    }
 
     var historyFetcherMock,
         developerResourcesHistoryFinderMock,
         $q,
         $timeout,
+        moment,
         historyAnalyzer;
 
     beforeEach(function () {
@@ -20,9 +28,10 @@ describe('historyAnalyzer Test', function () {
         });
     });
 
-    beforeEach(inject(function (_$q_, _$timeout_, _historyAnalyzer_) {
+    beforeEach(inject(function (_$q_, _$timeout_, _moment_, _historyAnalyzer_) {
         $q = _$q_;
         $timeout = _$timeout_;
+        moment = _moment_;
         historyAnalyzer = _historyAnalyzer_;
     }));
 
@@ -56,11 +65,25 @@ describe('historyAnalyzer Test', function () {
         });
 
         it('returns only one visit ok', function (done) {
+            var time = moment().toDate().valueOf();
             developerResources = [
-                {url: 'https://docs.angularjs.org/api', title: 'AngularJS', siteType: 'test', time: 1413735836683.803}
+                {
+                    url: 'https://docs.angularjs.org/api',
+                    title: 'AngularJS',
+                    siteType: 'test',
+                    time: time
+                }
             ];
             var expectedVisits = [
-                {title: 'AngularJS', url: 'https://docs.angularjs.org/api', classKind: 'test', spacer: false, needsDateHeader: true, friendlyTime:'6:23 PM', friendlyDate: 'Sunday, October 19 2014'}
+                {
+                    title: 'AngularJS',
+                    url: 'https://docs.angularjs.org/api',
+                    classKind: 'test',
+                    spacer: false,
+                    needsDateHeader: true,
+                    friendlyTime: getFriendlyTime(time),
+                    friendlyDate: getFriendlyDate(time)
+                }
             ];
 
             historyAnalyzer.getDeveloperResources().then(
@@ -73,13 +96,31 @@ describe('historyAnalyzer Test', function () {
         });
 
         it('return two visits in same day under separator threshold does not have separator', function (done) {
+            var time1 = moment().toDate().valueOf(),
+                time2 = moment().subtract(5, 'minutes').toDate().valueOf();
             developerResources = [
-                {url: 'https://docs.angularjs.org/api', title: 'AngularJS', siteType: 'test', time: 1413735836683.803},
-                {url: 'https://docs.angularjs.org/api', title: 'AngularJS', siteType: 'test', time: 1413735846683.803}
+                {url: 'https://docs.angularjs.org/api', title: 'AngularJS', siteType: 'test', time: time1},
+                {url: 'https://docs.angularjs.org/api', title: 'AngularJS', siteType: 'test', time: time2}
             ];
             var expectedVisits = [
-                {title: 'AngularJS', url: 'https://docs.angularjs.org/api', classKind: 'test', spacer: false, needsDateHeader: true, friendlyTime:'6:23 PM', friendlyDate: 'Sunday, October 19 2014'},
-                {title: 'AngularJS', url: 'https://docs.angularjs.org/api', classKind: 'test', spacer: false, needsDateHeader: false, friendlyTime:'6:24 PM', friendlyDate: 'Sunday, October 19 2014'}
+                {
+                    title: 'AngularJS',
+                    url: 'https://docs.angularjs.org/api',
+                    classKind: 'test',
+                    spacer: false,
+                    needsDateHeader: true,
+                    friendlyTime: getFriendlyTime(time1),
+                    friendlyDate: getFriendlyDate(time1)
+                },
+                {
+                    title: 'AngularJS',
+                    url: 'https://docs.angularjs.org/api',
+                    classKind: 'test',
+                    spacer: false,
+                    needsDateHeader: false,
+                    friendlyTime: getFriendlyTime(time2),
+                    friendlyDate: getFriendlyDate(time2)
+                }
             ];
 
             historyAnalyzer.getDeveloperResources().then(
@@ -92,13 +133,41 @@ describe('historyAnalyzer Test', function () {
         });
 
         it('return two visits in same day over separator threshold must have separator', function (done) {
+            var time1 = moment().toDate().valueOf(),
+                time2 = moment().subtract(35, 'minutes').toDate().valueOf();
             developerResources = [
-                {url: 'https://docs.angularjs.org/api', title: 'AngularJS', siteType: 'test', time: 1413735836683.803},
-                {url: 'https://docs.angularjs.org/api', title: 'AngularJS', siteType: 'test', time: 1413737836683.803}
+                {
+                    url: 'https://docs.angularjs.org/api',
+                    title: 'AngularJS',
+                    siteType: 'test',
+                    time: time1
+                },
+                {
+                    url: 'https://docs.angularjs.org/api',
+                    title: 'AngularJS',
+                    siteType: 'test',
+                    time: time2
+                }
             ];
             var expectedVisits = [
-                {title: 'AngularJS', url: 'https://docs.angularjs.org/api', classKind: 'test', spacer: false, needsDateHeader: true, friendlyTime:'6:23 PM', friendlyDate: 'Sunday, October 19 2014'},
-                {title: 'AngularJS', url: 'https://docs.angularjs.org/api', classKind: 'test', spacer: true, needsDateHeader: false, friendlyTime:'6:57 PM', friendlyDate: 'Sunday, October 19 2014'}
+                {
+                    title: 'AngularJS',
+                    url: 'https://docs.angularjs.org/api',
+                    classKind: 'test',
+                    spacer: false,
+                    needsDateHeader: true,
+                    friendlyTime: getFriendlyTime(time1),
+                    friendlyDate: getFriendlyDate(time1)
+                },
+                {
+                    title: 'AngularJS',
+                    url: 'https://docs.angularjs.org/api',
+                    classKind: 'test',
+                    spacer: true,
+                    needsDateHeader: false,
+                    friendlyTime: getFriendlyTime(time2),
+                    friendlyDate: getFriendlyDate(time2)
+                }
             ];
 
             historyAnalyzer.getDeveloperResources().then(
@@ -111,13 +180,41 @@ describe('historyAnalyzer Test', function () {
         });
 
         it('return two visits in different day does not have separators and have both header', function (done) {
+            var time1 = moment().toDate().valueOf(),
+                time2 = moment().subtract(1, 'day').toDate().valueOf();
             developerResources = [
-                {url: 'https://docs.angularjs.org/api', title: 'AngularJS', siteType: 'test', time: 1413735836683.803},
-                {url: 'https://docs.angularjs.org/api', title: 'AngularJS', siteType: 'test', time: 1413835836683.803}
+                {
+                    url: 'https://docs.angularjs.org/api',
+                    title: 'AngularJS',
+                    siteType: 'test',
+                    time: time1
+                },
+                {
+                    url: 'https://docs.angularjs.org/api',
+                    title: 'AngularJS',
+                    siteType: 'test',
+                    time: time2
+                }
             ];
             var expectedVisits = [
-                {title: 'AngularJS', url: 'https://docs.angularjs.org/api', classKind: 'test', spacer: false, needsDateHeader: true, friendlyTime:'6:23 PM', friendlyDate: 'Sunday, October 19 2014'},
-                {title: 'AngularJS', url: 'https://docs.angularjs.org/api', classKind: 'test', spacer: false, needsDateHeader: true, friendlyTime:'10:10 PM', friendlyDate: 'Monday, October 20 2014'}
+                {
+                    title: 'AngularJS',
+                    url: 'https://docs.angularjs.org/api',
+                    classKind: 'test',
+                    spacer: false,
+                    needsDateHeader: true,
+                    friendlyTime: getFriendlyTime(time1),
+                    friendlyDate: getFriendlyDate(time1)
+                },
+                {
+                    title: 'AngularJS',
+                    url: 'https://docs.angularjs.org/api',
+                    classKind: 'test',
+                    spacer: false,
+                    needsDateHeader: true,
+                    friendlyTime: getFriendlyTime(time2),
+                    friendlyDate: getFriendlyDate(time2)
+                }
             ];
 
             historyAnalyzer.getDeveloperResources().then(
